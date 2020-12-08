c17d6394072d51dc01dfa9921fe73f3e194b099c
Yana Gushlevskaya <yanagushlevskaya@Yanas-MacBook-Pro.local>
2020-12-08 23:05:44 +0200

    fix route

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Route, BrowserRouter, Redirect } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/">
-        <Redirect exact to="/employees" />
-        <Route path="/employees" component={Employees} />
+      <Route path="/Yalantis-test">
+        <Redirect exact to="/Yalantis-test/employees" />
+        <Route path="/Yalantis-test/employees" component={Employees} />
       </Route>
     </BrowserRouter>
   );
